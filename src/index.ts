00c1b0cae0e3997b23c1d8995c2b817d87f56055
rgalinas26 <52674802+rgalinas26@users.noreply.github.com>
2019-09-17 15:50:12 -0400

    End of tuesday

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { add } from './math';
+const num1 = document.getElementById("num1") as HTMLInputElement;
+const num2 = document.getElementById("num2") as HTMLInputElement;
+const addBtn = document.getElementById('add');
 
+const answer = document.getElementById("answer") as HTMLSpanElement;
+
+addBtn.addEventListener('click', function () {
+    const sum = add(num1.valueAsNumber, num2.valueAsNumber);
+
+    answer.innerText = sum.toString();
+})
